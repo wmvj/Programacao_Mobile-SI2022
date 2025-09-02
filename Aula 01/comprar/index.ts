e3b3c478466a27e2e9471d8f7b7db599d101a93e
wmvj <wmvj@unifesspa.edu.br>
2025-09-01 23:43:49 -0300

    Update melhoria na estrutura

---
+++ Aula 01/comprar/index.ts
@@ -0,0 +1,8 @@
+import { registerRootComponent } from 'expo';
+
+import {Home} from './src/app/Home';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(Home);

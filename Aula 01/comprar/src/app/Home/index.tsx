e2332a8248dd14d5f44427eb972612c9b8e52b98
wmvj <wmvj@unifesspa.edu.br>
2025-09-02 23:33:10 -0300

    add componentes

--- Aula 01/comprar/src/app/Home/index.tsx
+++ Aula 01/comprar/src/app/Home/index.tsx
@@ -1,11 +1,36 @@
-import { Text, View } from "react-native"
+import { Text, View, Image, TouchableOpacity } from "react-native"
 
 import { styles } from "./styles"
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
+import { Filter } from "@/components/Filter"
+import { FilterStatus } from "@/types/FilterStatus"
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
+      {/* <Text style={styles.text}>Hello, World!</Text> */}
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
     </View>
   )
 }
--- Aula 01/comprar/src/app/Home/styles.ts
+++ Aula 01/comprar/src/app/Home/styles.ts
@@ -4,11 +4,50 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#d0d2d8',
+    paddingTop: 62,
+    // padding: 24,
+    // gap: 24,
   },
-  text: {
-    fontSize: 32,
-    fontWeight: 700,
-    color: 'red',
+//   text: {
+//     fontSize: 32,
+//     fontWeight: 700,
+//     color: 'red',
+//   },
+  logo:{
+   height: 34,
+   width: 134, 
   },
+  form: {
+    width: '100%',
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    paddingTop: 32,
+    marginTop: 24,
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#E4E6EC',
+    paddingBottom: 12,
+  },
+  clearButton: {
+    marginLeft: 'auto',
+  },
+  clearText: {
+    fontSize: 12,
+    color: '#828282',
+    fontWeight: 600,
+  }
 });

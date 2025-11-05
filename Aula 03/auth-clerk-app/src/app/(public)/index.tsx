479b1eab7ffdcbc3bdb09105aaf9a300053a9b78
wmvj <wmvj@unifesspa.edu.br>
2025-11-05 11:30:37 -0300

    add pagina publica

--- Aula 03/auth-clerk-app/src/app/(public)/index.tsx
+++ Aula 03/auth-clerk-app/src/app/(public)/index.tsx
@@ -1,9 +1,31 @@
+import { Button } from "@/components/Button";
+import { useState } from "react";
 import { Text, View } from "react-native";
+import {useAuth} from "@clerk/clerk-expo"
 
 export default function SingIn(){
+
+    const [isLoading, setIsLoading] = useState()
+    const googleAuth =  useAuth({strategy: "oauth_google"})
+
+    async function signInWithGoogle() {
+        try {
+            // resolver a criação desta função para usar a conta do google com clerk
+        } catch (error) {
+            
+        }
+    }
+
+
     return(
         <View>
-            <Text></Text>           
+            <Text></Text>
+            <Button
+                icon="logo-google"
+                title="Entrar com o Google"
+                onPress={signInWithGoogle}
+                isLoading={isLoading}
+            />           
         </View>
     )
 }

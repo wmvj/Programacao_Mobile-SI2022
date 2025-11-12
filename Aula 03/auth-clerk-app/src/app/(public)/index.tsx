8b4de65e3b6ebedc37f90b8e1500b33d1cf9cf3f
wmvj <wmvj@unifesspa.edu.br>
2025-11-12 09:50:23 -0300

    update autenticacao com google

--- Aula 03/auth-clerk-app/src/app/(public)/index.tsx
+++ Aula 03/auth-clerk-app/src/app/(public)/index.tsx
@@ -1,25 +1,49 @@
 import { Button } from "@/components/Button";
-import { useState } from "react";
-import { Text, View } from "react-native";
-import {useAuth} from "@clerk/clerk-expo"
+import { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Linking } from "react-native";
+import {useSSO} from "@clerk/clerk-expo"
+import * as Liking from "expo-linking"
+import * as WebBrowser from "expo-web-browser"
+import {router} from "expo-router"
+
+WebBrowser.maybeCompleteAuthSession()
 
 export default function SingIn(){
 
-    const [isLoading, setIsLoading] = useState()
-    const googleAuth =  useAuth({strategy: "oauth_google"})
+    const [isLoading, setIsLoading] = useState(false)
+    const googleAuth =  useSSO()
 
     async function signInWithGoogle() {
         try {
+
+            setIsLoading(true)
+            const redirectUrl = Liking.createURL("/")
+            const oAuthFlow = await googleAuth.startSSOFlow()
             // resolver a criação desta função para usar a conta do google com clerk
-        } catch (error) {
             
+            if (oAuthFlow.authSessionResult?.type === 'success'){
+                if(oAuthFlow.setActive){
+                    await oAuthFlow.setActive({session: oAuthFlow.createdSessionId})
+                }
+            } else {
+                setIsLoading(false)
+            }
+        } catch (error) {
+            console.log(error)
+            setIsLoading(false)
         }
     }
 
+    useEffect(() => {
+        WebBrowser.warmUpAsync()
+        return () => {
+            WebBrowser.coolDownAsync()
+        }
+    },[])
 
     return(
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Entrar</Text>
             <Button
                 icon="logo-google"
                 title="Entrar com o Google"
@@ -29,3 +53,17 @@ export default function SingIn(){
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 32,
+        justifyContent: 'center',
+        gap: 12
+    },
+
+    text: {
+        fontSize: 12,
+        fontWeight: 'bold'
+    }
+})
